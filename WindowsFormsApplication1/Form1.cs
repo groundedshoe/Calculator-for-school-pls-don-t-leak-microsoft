7435414bd2edf050c90d793f65099f5c88e35082
groundedshoe <kasper.gillbergkling@gmail.com>
2018-12-13 09:24:27 +0100

    fixed variable naming

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -16,37 +16,42 @@ namespace WindowsFormsApplication1
         string MidTerm = ""; // tar det som ska hända
         string RightTerm = ""; // argumentet för det som ska hända
         bool TermState = false; // villken term som kan redigeras representerar denna. false är vänster och true är höger
-        bool deg = false; // för sin/cos/tan då man använder antingen grader eller radiens. false är radiens
+        bool Deg = false; // för sin/cos/tan då man använder antingen grader eller radiens. false är radiens
         public Form1()
         {
             InitializeComponent();
         }
         private void button1_Click(object sender, EventArgs e) { // alla knappar leder hit
-            setags((sender as Button).Text); // gör typ allt
-            updatelabel(); // updaterar texten
+            SetTerms((sender as Button).Text);//skickar namnet av knappen till SetTerms
+            UpdateLabel(); // updaterar texten
         }
-        void updatelabel() { // något som används ofta
+        void UpdateLabel() { // uppdaterar texten
             label1.Text = LeftTerm + MidTerm + RightTerm;
         }
-        void setags(string arg) {
+        /// <summary>
+        /// sätter alla variabler till det de ska vara när vi startar Resolve()
+        /// Hanterar också när Resolve() ska starta
+        /// altså en god funktion
+        /// </summary>
+        void SetTerms(string Arg) {
             int v;
-            if (Int32.TryParse(arg,out v)) { // kollar ifall den är ett heltal
+            if (Int32.TryParse(Arg,out v)) { // kollar ifall den är ett heltal
                 if (TermState) {//ifall sant kollar den villken term används och lägger till
                     if(RightTerm == "0") { // ser till att det inte blir tal med 0 först
-                        RightTerm = arg;
+                        RightTerm = Arg;
                     }else {
-                        RightTerm += arg;
+                        RightTerm += Arg;
                     }
                 }else {
                     if(LeftTerm == "0") {
-                        LeftTerm = arg;
+                        LeftTerm = Arg;
                     } else {
-                        LeftTerm += arg;
+                        LeftTerm += Arg;
                     }
                 }
-                return;//sparar minne
+               return;//sparar minne
             }
-            switch (arg) {//ifall detta inte går då så startar den ett switch case
+            switch (Arg) {//ifall detta inte går då så startar den ett switch case
                 case "+":
                 case "-":
                 case "/":
@@ -55,12 +60,12 @@ namespace WindowsFormsApplication1
                 case "^":
                 case "n√"://alla som kräver användar input på högertermen
                     if (MidTerm != "") {//ifall mitten termen är inte blank måste den bli klar innan något nytt argument
-                        resolve();//den använder argumenten för att annars skulle den bara försvinna
-                        MidTerm = arg;//sätter mitten till sig
+                        Resolve();//den använder argumenten för att annars skulle den bara försvinna
+                        MidTerm = Arg;//sätter mitten till sig
                         RightTerm = "0";
                         TermState = true;
                     }else {//bar sätter mitten till det det ska vara
-                        MidTerm = arg;
+                        MidTerm = Arg;
                         RightTerm = "0";
                         TermState = true;
                     }
@@ -70,12 +75,12 @@ namespace WindowsFormsApplication1
                 case "tan":
                 case "cos":
                 case "sin"://för allt som inte kräver mer än 1 argument
-                    resolve();
-                    MidTerm = arg;
-                    resolve();
+                    Resolve();
+                    MidTerm = Arg;
+                    Resolve();
                     break;
                 case "="://lika med. sätter bara ihop med argument
-                    resolve();
+                    Resolve();
                     break;
                 case ","://komma. Man kan inte ha mer än 1 komma sim ni vet så har vi lite har inte satser
                     if (TermState && !(RightTerm.Contains(','))) {
@@ -106,7 +111,7 @@ namespace WindowsFormsApplication1
                     }
                     break;
                 case "Deg/Rad"://byt mellan radiens och grader
-                    deg = !deg;
+                    Deg = !Deg;
                     break;
                 case "CE"://allt blir som vid starten(förutom grader/radiens)
                     LeftTerm = "0";
@@ -118,7 +123,7 @@ namespace WindowsFormsApplication1
             }
                 
         }
-        void resolve() {//ser till att kalkyleringen händer.
+        void Resolve() {//ser till att kalkyleringen händer.
             float a = 0;
             if ((float.TryParse(LeftTerm.Replace(",","") + RightTerm.Replace(",",""), out a))){//bara ifall du på något sätt fick ∞66 som float.parse inte tillåter
                 a = default(float);
@@ -160,14 +165,14 @@ namespace WindowsFormsApplication1
                         LeftTerm = (Math.Pow(float.Parse(LeftTerm), 1 / float.Parse(RightTerm))).ToString();
                         break;
                     case "tan"://trigonometriska funktionerna
-                        if (deg) {//ifall du använder grader
+                        if (Deg) {//ifall du använder grader
                             LeftTerm = (Math.Tan(float.Parse(LeftTerm) / 57.2957795)).ToString();
                         } else {
                             LeftTerm = (Math.Tan(float.Parse(LeftTerm))).ToString();
                         }
                         break;
                     case "cos":
-                        if (deg) {
+                        if (Deg) {
                             LeftTerm = (Math.Cos(float.Parse(LeftTerm) / 57.2957795)).ToString();
                         } else {
                             LeftTerm = (Math.Cos(float.Parse(LeftTerm))).ToString();
@@ -175,7 +180,7 @@ namespace WindowsFormsApplication1
                         break;
                     case "sin":
 
-                        if (deg) {
+                        if (Deg) {
                             LeftTerm = (Math.Sin(float.Parse(LeftTerm) / 57.2957795)).ToString();
                         } else {
                             LeftTerm = (Math.Sin(float.Parse(LeftTerm))).ToString();
@@ -197,10 +202,8 @@ namespace WindowsFormsApplication1
             }
         }
 
-        
-
         private void Form1_Load(object sender, EventArgs e) {
-            updatelabel();
+            UpdateLabel();
         }
     }
 }
